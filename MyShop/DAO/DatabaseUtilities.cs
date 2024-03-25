ec80adbd73f9ad22c6cc63a8c38b6629e935e1bb
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2024-03-25 12:55:59 +0700

    Init resourses + configuration window

---
+++ MyShop/DAO/DatabaseUtilities.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Data.SqlClient;
+
+namespace MyShop.DAO
+{
+    class DatabaseUtilities
+    {
+        private string _server;
+        private string _databaseName;
+        private string _user;
+        private string _password;
+        public static bool isSelectedDatabase = false;
+
+        private static DatabaseUtilities _instance = null;
+        SqlConnection _connection;
+        public static DatabaseUtilities getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DatabaseUtilities();
+            }
+            return _instance;
+        }
+
+        public DatabaseUtilities()
+        {
+            _server = "sqlexpress";
+            _databaseName = "MyShop";
+            _user = "sa";
+            _password = "123";
+            _connection = null;
+        }
+
+        public DatabaseUtilities(string server, string databaseName, string user, string password)
+        {
+            _server = server;
+            _databaseName = databaseName;
+            _user = user;
+            _password = password;
+
+            string connectionString = $"""
+                Server = {server};
+                User ID = {user}; Password={password};
+                Database = {databaseName};
+                TrustServerCertificate=True;
+                Trusted_Connection=yes;
+                """;
+
+            _connection = new SqlConnection(connectionString);
+
+            try
+            {
+                _connection.Open();
+                isSelectedDatabase = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Cannot connect to database. Reason: {ex.Message}");
+            }
+
+            _instance = this;
+        }
+
+        public SqlConnection connection { get { return _connection; } }
+    }
+}
---
+++ MyShop/UI/ConfigWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using MyShop.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyShop.UI
+{
+    /// <summary>
+    /// Interaction logic for ConfigWindow.xaml
+    /// </summary>
+    public partial class ConfigWindow : Window
+    {
+        public ConfigWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string servername = ServerTermTextBox.Text;
+            string username = UsernameTermTextBox.Text;
+            string password = PasswordTermTextBox.Text;
+            string databasename = DatabaseTermTextBox.Text;
+
+            new DatabaseUtilities(
+                  servername,
+                  databasename,
+                  username,
+                  password
+            );
+
+            if (DatabaseUtilities.isSelectedDatabase == true)
+            {
+                var loginWindow = new LoginWindow();
+                loginWindow.Show();
+                this.Close();
+            }
+
+        }
+    }
+}
